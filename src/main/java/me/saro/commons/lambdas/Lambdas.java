fb774e291492a211790d8d7a4ee547ebf36832c7
PARK Yong Seo <j@saro.me>
2018-09-23 16:24:15 +0900

    add Lambdas class and 1 test case

---
+++ src/main/java/me/saro/commons/lambdas/Lambdas.java
@@ -0,0 +1,107 @@
+package me.saro.commons.lambdas;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Lambdas Util
+ * @author		PARK Yong Seo
+ * @since		1.0.0
+ */
+public class Lambdas {
+	private Lambdas() {
+	}
+	
+	/**
+	 * throws Exception lambda to throws RuntimeException lambda
+	 * @param runnable
+	 * @return
+	 */
+	public static Runnable runtime(ThrowableRunnable runnable) {
+		return () -> {
+			try {
+				runnable.run();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+	
+	/**
+	 * throws Exception lambda to throws RuntimeException lambda
+	 * @param supplier
+	 * @return
+	 */
+	public static <R> Supplier<R> runtime(ThrowableSupplier<R> supplier) {
+		return () -> {
+			try {
+				return supplier.get();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+	
+	/**
+	 * throws Exception lambda to throws RuntimeException lambda
+	 * @param consumer
+	 * @return
+	 */
+	public static <T> Consumer<T> runtime(ThrowableConsumer<T> consumer) {
+		return t -> {
+			try {
+				consumer.accept(t);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+	
+	/**
+	 * throws Exception lambda to throws RuntimeException lambda
+	 * @param biConsumer
+	 * @return
+	 */
+	public static <T, U> BiConsumer<T, U> runtime(ThrowableBiConsumer<T, U> biConsumer) {
+		return (t, u) -> {
+			try {
+				biConsumer.accept(t, u);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+	
+	/**
+	 * throws Exception lambda to throws RuntimeException lambda
+	 * @param function
+	 * @return
+	 */
+	public static <T, R> Function<T, R> runtime(ThrowableFunction<T, R> function) {
+		return t -> {
+			try {
+				return function.apply(t);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+	
+	/**
+	 * throws Exception lambda to throws RuntimeException lambda
+	 * @param biFunction
+	 * @return
+	 */
+	public static <T, U, R> BiFunction<T, U, R> runtime(ThrowableBiFunction<T, U, R> biFunction) {
+		return (t, u) -> {
+			try {
+				return biFunction.apply(t, u);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+}
