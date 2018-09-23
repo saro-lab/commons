80d7182c48d4f08a072501c9b91d7dc6de0247d6
PARK Yong Seo <j@saro.me>
2018-09-24 06:28:59 +0900

    append web ignore textg

--- src/test/java/me/saro/commons/WebTest.java
+++ src/test/java/me/saro/commons/WebTest.java
@@ -7,6 +7,12 @@ public class WebTest {
 	@Test
     public void test() throws Exception {
 		
+		// ssl cert ignore test
+		//String sslErrorUrl = "https://cert-error.saro.me";
+		//assertFalse(Web.get(sslErrorUrl).toPlainText().isSuccess());
+		//assertTrue(Web.get(sslErrorUrl).setIgnoreCertificate(true).toPlainText().isSuccess());
+		
+		
     }
 	
 }
