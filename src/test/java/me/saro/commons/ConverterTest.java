6b78a518187531fe86f5c457f27ff076fab2cb4c
PARK Yong Seo <j@saro.me>
2018-09-25 03:16:00 +0900

    test code for converter

--- src/test/java/me/saro/commons/ConverterTest.java
+++ src/test/java/me/saro/commons/ConverterTest.java
@@ -1,5 +1,9 @@
 package me.saro.commons;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 
 public class ConverterTest {
@@ -7,6 +11,23 @@ public class ConverterTest {
 	@Test
 	public void test() throws Exception {
 
+		assertEquals("123", Converter.asList("1", "2", "3").stream().collect(Collectors.joining()));
+		
+		assertEquals(5, Converter.splitCsvLine("aaa,bbb,ccc,\"ddd,eee\",fff").length);
+		
+		assertEquals("00ff", Converter.toHex(new byte[] {0, -1}));
+		
+		// Converter.HASH_ALGORITHM_SHA3* minimum java version is 10 
+		assertEquals(
+			"2ce5bebfa51bf5b222a5c8977d3c1d37875703d3",
+			Converter.toHex(Converter.toHash(Converter.HASH_ALGORITHM_SHA1, "SARO"))
+		);
+	
+		assertEquals(
+			"a1b2c3",
+			Converter.<String, Integer>toMap("a", 1, "b", 2, "c", 3).entrySet().stream()
+			.map(e -> e.getKey() + e.getValue()).collect(Collectors.joining())
+		);
 	}
 	
 }
