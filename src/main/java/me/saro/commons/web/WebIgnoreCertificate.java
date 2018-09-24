b40482b834201650c8aef9209a225f23f6cd6b3f
PARK Yong Seo <j@saro.me>
2018-09-24 17:47:36 +0900

    change web ssl ignore logic

--- src/main/java/me/saro/commons/web/WebIgnoreCertificate.java
+++ src/main/java/me/saro/commons/web/WebIgnoreCertificate.java
@@ -20,56 +20,45 @@ import javax.net.ssl.X509TrustManager;
 public class WebIgnoreCertificate {
 	private WebIgnoreCertificate() {
 	}
-	
+
 	final static TrustManager[] ALL_TRUST_MANAGER;
-	
 	final static HostnameVerifier IGNORE_HOSTNAME_VERIFIER;
-    
-    final static SSLContext ALL_TRUST_SSLCONTEXT;
-    
-    static {
-    	ALL_TRUST_MANAGER = new TrustManager[] {
-    			new X509TrustManager() {
-    				@Override
-    				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-    					return null;
-    				}
-    				@Override
-    				public void checkClientTrusted(X509Certificate[] certs, String authType) {
-    				}
-    				@Override
-    				public void checkServerTrusted(X509Certificate[] certs, String authType) {
-    				}
-    			}
-    	};
-    	
-    	SSLContext sslContext = null;
-    	try {
-			sslContext = SSLContext.getInstance("SSL");
-			sslContext.init(null, ALL_TRUST_MANAGER, new java.security.SecureRandom());
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (KeyManagementException e) {
-			e.printStackTrace();
-		}
-    	
-    	ALL_TRUST_SSLCONTEXT = sslContext;
-    	
-    	IGNORE_HOSTNAME_VERIFIER = new HostnameVerifier() {
-    		@Override
-            public boolean verify(String hostname, SSLSession session) {
-                return true;
-            }
-        };
-    }
-    
-    /**
-     * casing ignore certificate to HttpURLConnection
-     * @param connection
-     */
-	public static void ignoreCertificate(HttpURLConnection connection) {
-		((HttpsURLConnection)connection).setSSLSocketFactory(ALL_TRUST_SSLCONTEXT.getSocketFactory());
+
+	static {
+		ALL_TRUST_MANAGER = new TrustManager[] {
+				new X509TrustManager() {
+					@Override
+					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+						return null;
+					}
+					@Override
+					public void checkClientTrusted(X509Certificate[] certs, String authType) {
+					}
+					@Override
+					public void checkServerTrusted(X509Certificate[] certs, String authType) {
+					}
+				}
+		};
+
+		IGNORE_HOSTNAME_VERIFIER = new HostnameVerifier() {
+			@Override
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		};
+	}
+
+	/**
+	 * casing ignore certificate to HttpURLConnection
+	 * @param connection
+	 * @throws KeyManagementException 
+	 * @throws NoSuchAlgorithmException 
+	 */
+	public static void ignoreCertificate(HttpURLConnection connection) throws KeyManagementException, NoSuchAlgorithmException {
+		SSLContext sslContext = SSLContext.getInstance("SSL");
+		sslContext.init(null, ALL_TRUST_MANAGER, new java.security.SecureRandom());
+		((HttpsURLConnection)connection).setSSLSocketFactory(sslContext.getSocketFactory());
 		((HttpsURLConnection)connection).setHostnameVerifier(IGNORE_HOSTNAME_VERIFIER);
-		
+
 	}
 }
--- src/test/java/me/saro/commons/WebTest.java
+++ src/test/java/me/saro/commons/WebTest.java
@@ -1,6 +1,11 @@
 package me.saro.commons;
 
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+//import me.saro.commons.web.Web;
 
 public class WebTest {
 	
@@ -8,9 +13,9 @@ public class WebTest {
     public void test() throws Exception {
 		
 		// ssl cert ignore test
-		//String sslErrorUrl = "https://cert-error.saro.me";
-		//assertFalse(Web.get(sslErrorUrl).toPlainText().isSuccess());
-		//assertTrue(Web.get(sslErrorUrl).setIgnoreCertificate(true).toPlainText().isSuccess());
+//		String sslErrorUrl = "https://cert-error.saro.me";
+//		assertFalse(Web.get(sslErrorUrl).toPlainText().isSuccess());
+//		assertTrue(Web.get(sslErrorUrl).setIgnoreCertificate(true).toPlainText().isSuccess());
 		
 		
     }
