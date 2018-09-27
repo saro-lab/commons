22640078026f763db0ad8db9ee69ebab33e55654
PARK Yong Seo <j@saro.me>
2018-09-27 12:45:11 +0900

    append ThrowablePredicate class

---
+++ src/main/java/me/saro/commons/lambdas/ThrowablePredicate.java
@@ -0,0 +1,33 @@
+package me.saro.commons.lambdas;
+
+/**
+ * Throwable <code>Predicate</code>
+ * 
+ * @param <T>
+ * input parameter type T
+ * 
+ * @see
+ * java.util.function.Predicate
+ * 
+ * @author
+ * PARK Yong Seo
+ * 
+ * @since
+ * 1.0.0
+ */
+@FunctionalInterface
+public interface ThrowablePredicate<T> {
+	/**
+	 * @see
+	 * java.util.function.Predicate
+	 * 
+	 * @param t
+	 * input parameter type T
+	 * 
+	 * @return
+	 * test return
+	 * 
+	 * @throws Exception
+	 */
+	boolean test(T t) throws Exception;
+}
