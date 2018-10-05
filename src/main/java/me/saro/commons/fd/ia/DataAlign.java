e75498710f682c4a86b4ea8bfb73630341111cb8
PARK Yong Seo <j@saro.me>
2018-10-05 17:28:48 +0900

    create part of the FixedData

---
+++ src/main/java/me/saro/commons/fd/ia/DataAlign.java
@@ -0,0 +1,10 @@
+package me.saro.commons.fd.ia;
+
+/**
+ * 
+ * @author		PARK Yong Seo
+ * @since		0.2
+ */
+public enum DataAlign {
+	left, right
+}
---
+++ src/test/java/me/saro/commons/FixedDataTest.java
@@ -0,0 +1,12 @@
+package me.saro.commons;
+
+import org.junit.jupiter.api.Test;
+
+public class FixedDataTest {
+
+	@Test
+	public void test() throws Exception {
+
+	}
+
+}
