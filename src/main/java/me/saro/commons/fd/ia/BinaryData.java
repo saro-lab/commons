3fd605468def318941052bc50a3734b815d6f359
PARK Yong Seo <j@saro.me>
2018-10-05 17:50:58 +0900

    add dataflow

--- src/main/java/me/saro/commons/fd/ia/BinaryData.java
+++ src/main/java/me/saro/commons/fd/ia/BinaryData.java
@@ -11,4 +11,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface BinaryData {
 	int offset() default -1;
+	DataFlow flow() default DataFlow.all;
 }
---
+++ src/main/java/me/saro/commons/fd/ia/DataFlow.java
@@ -0,0 +1,12 @@
+package me.saro.commons.fd.ia;
+
+/**
+ * 
+ * @author		PARK Yong Seo
+ * @since		0.2
+ */
+public enum DataFlow {
+	read,
+	write,
+	all
+}
