e95ec40d210314a373ef322885035ca64f419e9f
PARK Yong Seo <j@saro.me>
2018-10-13 08:08:21 +0900

    add naming convention

--- src/test/java/me/saro/commons/ConverterTest.java
+++ src/test/java/me/saro/commons/ConverterTest.java
@@ -4,11 +4,13 @@ package me.saro.commons;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
 import me.saro.commons.converter.HashAlgorithm;
+import me.saro.commons.converter.NamingConvention;
 
 public class ConverterTest {
 
@@ -32,6 +34,14 @@ public class ConverterTest {
 	public void toHash() {
 		// Converter.HASH_ALGORITHM_SHA3* minimum java version is 10 
 		assertEquals("2ce5bebfa51bf5b222a5c8977d3c1d37875703d3", Converter.toHex(Converter.toHash(HashAlgorithm.SHA1, "SARO")));
+	}
+	
+	@Test
+	public void namingConvention() {
+		assertEquals("fooBar", Converter.namingConvention(NamingConvention.camelCase, Arrays.asList("foo", "bar")));
+		assertEquals("foo-bar", Converter.namingConvention(NamingConvention.dashes, Arrays.asList("foo", "bar")));
+		assertEquals("FooBar", Converter.namingConvention(NamingConvention.pascalCase, Arrays.asList("foo", "bar")));
+		assertEquals("foo_bar", Converter.namingConvention(NamingConvention.underscores, Arrays.asList("foo", "bar")));
 	}
 	
 }
