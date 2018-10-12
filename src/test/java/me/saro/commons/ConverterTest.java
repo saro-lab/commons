b6de18b82b920bb31d74919b37de76ce4dba4549
PARK Yong Seo <j@saro.me>
2018-10-13 06:32:34 +0900

    edit converter test case

--- src/test/java/me/saro/commons/ConverterTest.java
+++ src/test/java/me/saro/commons/ConverterTest.java
@@ -8,6 +8,8 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
+import me.saro.commons.converter.HashAlgorithm;
+
 public class ConverterTest {
 
 	@Test
@@ -18,18 +20,18 @@ public class ConverterTest {
 		assertEquals(5, Converter.splitCsvLine("aaa,bbb,ccc,\"ddd,eee\",fff").length);
 		
 		assertEquals("00ff", Converter.toHex(new byte[] {0, -1}));
-		
-		// Converter.HASH_ALGORITHM_SHA3* minimum java version is 10 
-		assertEquals(
-			"2ce5bebfa51bf5b222a5c8977d3c1d37875703d3",
-			Converter.toHex(Converter.toHash(Converter.HASH_ALGORITHM_SHA1, "SARO"))
-		);
 	
 		assertEquals(
 			"a1b2c3",
 			Converter.<String, Integer>toMap("a", 1, "b", 2, "c", 3).entrySet().stream()
 			.map(e -> e.getKey() + e.getValue()).collect(Collectors.joining())
 		);
+	}
+	
+	@Test
+	public void toHash() {
+		// Converter.HASH_ALGORITHM_SHA3* minimum java version is 10 
+		assertEquals("2ce5bebfa51bf5b222a5c8977d3c1d37875703d3", Converter.toHex(Converter.toHash(HashAlgorithm.SHA1, "SARO")));
 	}
 	
 }
