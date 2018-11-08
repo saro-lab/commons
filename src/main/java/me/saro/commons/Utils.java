a8009eb4cb1ac202f1629b065877b7ed76245e0c
PARK Yong Seo <j@saro.me>
2018-11-09 02:33:46 +0900

    remove unused import

--- src/main/java/me/saro/commons/Utils.java
+++ src/main/java/me/saro/commons/Utils.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
