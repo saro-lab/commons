09928ba517bd768f42c9ee0fe73814b348323a1a
PARK Yong Seo <j@saro.me>
2018-11-10 21:39:16 +0900

    comment change

--- src/test/java/me/saro/commons/FilesTest.java
+++ src/test/java/me/saro/commons/FilesTest.java
@@ -13,15 +13,15 @@ public class FilesTest {
     
     public void remove() {
         
-        // 현재로부터 24시간 이전
+        // before 24 hour
         long before24hour = DateFormat.now().addHours(-24).getTimeInMillis();
         
         Files
-            /// 경로에 모든 파일을 가져옴
+            // get files in /testpath
             .streamFiles("/testpath")
-            // 24시간 보다 오래된 데이터만 가져옴
+            // filter : older then 24 hours
             .filter(Files.attributesFilter(attr -> attr.creationTime().toMillis() < before24hour))
-            // 삭제
+            // delete
             .forEach(File::delete);
     }
 }
