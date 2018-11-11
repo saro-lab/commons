0228484211b92149fd809c97dbb1baab2f9df419
PARK Yong Seo <j@saro.me>
2018-11-11 20:34:02 +0900

    test tool

---
+++ src/main/java/me/saro/commons/Tests.java
@@ -0,0 +1,31 @@
+package me.saro.commons;
+
+import me.saro.commons.function.ThrowableRunnable;
+
+/**
+ * test class
+ * @author      PARK Yong Seo
+ * @since       1.0
+ */
+public class Tests {
+    private Tests() {
+    }
+    
+    /**
+     * timestamp<br>
+     * check time of the runnable
+     * @param runnable
+     * @return
+     */
+    public static long timestamp(ThrowableRunnable runnable) {
+        long ts = System.currentTimeMillis();
+        try {
+            runnable.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ts = System.currentTimeMillis() - ts;
+        System.out.println("timestamp : " + ts);
+        return ts;
+    }
+}
