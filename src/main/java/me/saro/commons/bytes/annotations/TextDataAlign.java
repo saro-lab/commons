57918c0cf1d6903026fff30bd2614552a48c5f28
PARK Yong Seo <j@saro.me>
2018-11-11 20:30:45 +0900

    text align

---
+++ src/main/java/me/saro/commons/bytes/annotations/TextDataAlign.java
@@ -0,0 +1,19 @@
+package me.saro.commons.bytes.annotations;
+
+/**
+ *  Text Align
+ * @author      PARK Yong Seo
+ * @since       1.0
+ */
+public enum TextDataAlign {
+    /**
+     * align left
+     * [abc       ] is [abc]
+     */
+    left,
+    /**
+     * align right
+     * [      abc] is [abc]
+     */
+    right
+}
