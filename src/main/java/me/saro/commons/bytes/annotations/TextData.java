abc4c7ba61024abacb12c6fcafc53867fb7bf4d8
PARK Yong Seo <j@saro.me>
2018-11-11 20:35:30 +0900

    text data

---
+++ src/main/java/me/saro/commons/bytes/annotations/TextData.java
@@ -0,0 +1,54 @@
+package me.saro.commons.bytes.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *  text data in FixedDataFormat
+ * @author      PARK Yong Seo
+ * @since       1.0
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TextData {
+    
+    /**
+     * offset
+     * @return
+     */
+    int offset() default -1;
+    
+    /**
+     * byte length
+     * @return
+     */
+    int length() default -1;
+    
+    /**
+     * base fill
+     * @return
+     */
+    char fill() default ' ';
+    
+    /**
+     * is unsigned number
+     * @return
+     */
+    boolean unsigned() default false;
+    
+    /**
+     * charset
+     * @return
+     */
+    String charset() default "UTF-8";
+    
+    /**
+     * align
+     * @return
+     * @see
+     * TextDataAlign
+     */
+    TextDataAlign align() default TextDataAlign.left;
+}
