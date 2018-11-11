cedbe6e97c120799e15b8c4bc8fd45972180ffad
PARK Yong Seo <j@saro.me>
2018-11-11 13:40:00 +0900

    change test class name

--- src/test/java/me/saro/commons/DateFormatTest.java
+++ src/test/java/me/saro/commons/DateFormatTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class DataFormatTest {
+public class DateFormatTest {
 
     @Test
     public void test() throws Exception {
