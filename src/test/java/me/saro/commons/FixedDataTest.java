8e1ff8e53b89a28fc99e1dd9466d9fde917d6e6c
PARK Yong Seo <j@saro.me>
2018-11-13 15:56:13 +0900

    binary test

--- src/test/java/me/saro/commons/FixedDataTest.java
+++ src/test/java/me/saro/commons/FixedDataTest.java
@@ -1,8 +1,12 @@
 package me.saro.commons;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import me.saro.commons.bytes.Bytes;
 import me.saro.commons.bytes.FixedDataFormat;
 import me.saro.commons.bytes.annotations.BinaryData;
@@ -11,81 +15,64 @@ import me.saro.commons.bytes.annotations.FixedData;
 public class FixedDataTest {
 
     @Test
-    public void test() {
+    public void binary() {
+        FixedDataFormat<BinaryStruct> format = FixedDataFormat.create(BinaryStruct.class, () -> new BinaryStruct());
         
-        try {
-            fixeddata();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        BinaryStruct bs = new BinaryStruct((byte)-1, (short)321, 1234, 76543L, 2.1F, 3.6D, new byte[] {0x1f, 0x3b, 0x33});
         
+        byte[] bytes = format.toBytes(bs);
+        
+        assertEquals(bytes.length, 30);
+        
+        assertEquals(Bytes.toHex(bytes), "ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33");
+        
+        assertEquals(bs, format.toClass(bytes));
+        
+        
+        byte[] bytes2 = new byte[60];
+        format.bindBytes(bytes2, 0, bs);
+        format.bindBytes(bytes2, 30, bs);
+        
+        assertEquals(Bytes.toHex(bytes2), "ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33");
+        
+        assertEquals(bs, format.toClass(bytes2, 30));
     }
     
-    public void fixeddata() throws Exception {
-        FixedDataFormat<Abc> fd = FixedDataFormat.create(Abc.class);
-        byte[] aaa = "12345                                             ".getBytes();
-        
-        System.out.println(fd.toClass(aaa));
-        
-        System.out.println(Bytes.toHex(fd.toBytes(new Abc())));
-        
-        Tests.timestamp(() -> {
-          for (int i = 0 ; i < 10000 ; i++) {
-              //fd.toClass(aaa);
-              fd.toBytes(new Abc());
-          }
-        });
-    }
-
+//    @Test
+//    public void text() {
+//       
+//    }
+//    
+//    @Test
+//    public void mixed() {
+//       
+//    }
     
-    @Data @FixedData(size=8, fill=0)
-    public static class Abc {
-//        @TextData(offset=0, length=10, align=TextDataAlign.right)
-//        String c1;
+    @Data
+    @FixedData(size=30, fill=0)
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BinaryStruct {
         
-        @BinaryData(offset=4)
-        int a = -1;
+        @BinaryData(offset=0)
+        byte byteData;
         
-//        @TextData(offset=0, length=10)
-//        String a1;
-//        
-//        
-//        @TextData(offset=0, length=10)
-//        byte a2;
-//        
-//        @TextData(offset=0, length=10)
-//        short a3;
-//        
-//        @TextData(offset=0, length=10)
-//        int a4;
-//        
-//        @TextData(offset=0, length=10)
-//        long a5;
-//        
-//        @TextData(offset=0, length=10)
-//        float a6;
-//        
-//        @TextData(offset=0, length=10)
-//        double a7;
-//        
-//        @TextData(offset=0, length=10)
-//        Byte a12;
-//        
-//        @TextData(offset=0, length=10)
-//        Short a13;
-//        
-//        @TextData(offset=0, length=10)
-//        Integer a14;
-//        
-//        @TextData(offset=0, length=10)
-//        Long a15;
-//        
-//        @TextData(offset=0, length=10)
-//        Float a16;
-//        
-//        @TextData(offset=0, length=10)
-//        Double a17;
-//        
-//        String b1;
+        @BinaryData(offset=1)
+        short shortData;
+        
+        @BinaryData(offset=3)
+        int intData;
+        
+        @BinaryData(offset=7)
+        long longData;
+        
+        @BinaryData(offset=15)
+        float floatData;
+        
+        @BinaryData(offset=19)
+        double doubleData;
+        
+        @BinaryData(offset=27, arrayLength=3)
+        byte[] bytesData;
     }
 }
