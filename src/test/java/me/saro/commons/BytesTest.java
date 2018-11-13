efb352bed1aa591630f93c7d08b1ad8508f47d7f
PARK Yong Seo <j@saro.me>
2018-11-13 14:49:54 +0900

    byte test case

--- src/test/java/me/saro/commons/BytesTest.java
+++ src/test/java/me/saro/commons/BytesTest.java
@@ -1,11 +1,47 @@
 package me.saro.commons;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
+import me.saro.commons.bytes.Bytes;
 
 public class BytesTest {
     
     @Test
-    public void test() {
-        
+    public void shortTest() {
+        assertEquals(Bytes.toHex(Bytes.toBytes((short)32)), "0020");
+        assertEquals(Bytes.toHex(Bytes.toBytes((short)Short.parseShort("-1"))), "ffff");
+        assertEquals(Bytes.toShort(Bytes.toBytes((short)123)), (short)123);
+    }
+    
+    @Test
+    public void intTest() {
+        assertEquals(Bytes.toHex(Bytes.toBytes((int)32)), "00000020");
+        assertEquals(Bytes.toHex(Bytes.toBytes((int)-1)), "ffffffff");
+        assertEquals(Bytes.toInt(Bytes.toBytes(8080)), 8080);
+    }
+    
+    @Test
+    public void longTest() {
+        assertEquals(Bytes.toHex(Bytes.toBytes((long)32)), "0000000000000020");
+        assertEquals(Bytes.toHex(Bytes.toBytes(-1L)), "ffffffffffffffff");
+        assertEquals(Bytes.toLong(Bytes.toBytes(324L)), 324L);
+    }
+    
+    @Test
+    public void floatTest() {
+        float value = 32;
+        byte[] bytes = Bytes.toBytes(value);
+        float rocv = Bytes.toFloat(bytes);
+        assertEquals(value, rocv);
+    }
+    
+    @Test
+    public void dobuleTest() {
+        double value = 32;
+        byte[] bytes = Bytes.toBytes(value);
+        double rocv = Bytes.toDouble(bytes);
+        assertEquals(value, rocv);
     }
 }
