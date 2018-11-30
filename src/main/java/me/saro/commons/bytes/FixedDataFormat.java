69dec0912b6ef340be6174f6392f562492d1d367
PARK Yong Seo <j@saro.me>
2018-11-30 13:19:17 +0900

    import clear

--- src/main/java/me/saro/commons/bytes/FixedDataFormat.java
+++ src/main/java/me/saro/commons/bytes/FixedDataFormat.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
