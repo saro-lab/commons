6d18a1e9f55402744011050095e183c826377451
PARK Yong Seo <j@saro.me>
2018-12-07 12:54:56 +0900

    restore fixed data and remove split data

--- src/test/java/me/saro/commons/ReadmeTest.java
+++ src/test/java/me/saro/commons/ReadmeTest.java
@@ -8,7 +8,6 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 import me.saro.commons.bytes.Bytes;
-import me.saro.commons.bytes.DataFormat;
 import me.saro.commons.ftp.FTP;
 import me.saro.commons.ssh.SSHExecutor;
 import me.saro.commons.ssh.SSHShell;
@@ -33,7 +32,7 @@ public class ReadmeTest {
         System.out.println("# INSTANCE");
         System.out.println();
 
-        outClassInfo(DataFormat.class);
+        outClassInfo(FixedDataTest.class);
         outClassInfo(Web.class);
         outClassInfo(FTP.class);
         outClassInfo(SSHShell.class);
