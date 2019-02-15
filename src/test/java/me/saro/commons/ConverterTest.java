c4d3455ff4823cc9c413f957e7265103a13a9879
PARK Yong Seo <j@saro.me>
2019-02-15 16:53:15 +0900

    simple crypt

--- src/test/java/me/saro/commons/ConverterTest.java
+++ src/test/java/me/saro/commons/ConverterTest.java
@@ -2,8 +2,18 @@ package me.saro.commons;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.junit.jupiter.api.Test;
 
@@ -54,4 +64,20 @@ public class ConverterTest {
         assertEquals("foo_bar", Converter.namingConvention(NamingConvention.underscores, Arrays.asList("foo", "bar")));
     }
 
+    @Test
+    public void crypt() throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        
+        String charset = "UTF-8";
+        
+        String trens = "AES/CBC/PKCS5Padding";
+        Key key = new SecretKeySpec("abcdefg123456789".getBytes(), "AES");
+        byte[] iv = ")3ksu38s6Ad8D2^&".getBytes();
+        
+        String test = "abcdefg 가나다라 @#$!ㅇㄴ8732!$%";
+        
+        String en = Bytes.toHex(Converter.encrypt(trens, key, iv, test.getBytes(charset)));
+        String de = new String(Converter.decrypt(trens, key, iv, Bytes.toBytesByHex(en)), charset);
+        
+        assertEquals(test, de);
+    }
 }
