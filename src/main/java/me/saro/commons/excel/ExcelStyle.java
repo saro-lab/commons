62ec9e9af36519238a9d47f040270b10cd84eba2
PARK Yong Seo <j@saro.me>
2019-02-27 03:09:43 +0900

    excel

--- src/main/java/me/saro/commons/excel/ExcelStyle.java
+++ src/main/java/me/saro/commons/excel/ExcelStyle.java
@@ -2,8 +2,11 @@ package me.saro.commons.excel;
 
 import java.util.function.Consumer;
 
+import org.apache.poi.xssf.model.StylesTable;
+import org.apache.poi.xssf.usermodel.IndexedColorMap;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
@@ -21,13 +24,15 @@ public class ExcelStyle {
     final int startCellIndex;
     final int endRowIndex;
     final int endCellIndex;
+    final StylesTable styleTable;
     
-    public ExcelStyle(XSSFSheet sheet, int startRowIndex, int startCellIndex, int endRowIndex, int endCellIndex) {
+    public ExcelStyle(XSSFSheet sheet, int startRowIndex, int startCellIndex, int endRowIndex, int endCellIndex, StylesTable styleTable) {
         this.sheet = sheet;
         this.startCellIndex = startCellIndex;
         this.endCellIndex = endCellIndex;
         this.startRowIndex = startRowIndex;
         this.endRowIndex = endRowIndex;
+        this.styleTable = styleTable;
     }
     
     public void forEach(Consumer<XSSFCellStyle> consumer) {
@@ -38,5 +43,15 @@ public class ExcelStyle {
                 consumer.accept(cell.getCellStyle());
             }
         }
+    }
+    
+    public void fontSize(short size) {
+        forEach(e -> e.getFont().setFontHeight(size));
+    }
+    
+    public void fontColor(int r, int g, int b) {
+        IndexedColorMap icm = styleTable.getIndexedColors();
+        XSSFColor grey = new XSSFColor(new java.awt.Color(r, g, b), icm);
+        forEach(e -> e.getFont().setColor(grey));
     }
 }
