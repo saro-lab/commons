9476dc5e5b0dd8c7925a2ccb9c771afbe619f185
PARK Yong Seo <j@saro.me>
2019-03-02 07:38:49 +0900

    lite test

--- src/test/java/me/saro/commons/WebTest.java
+++ src/test/java/me/saro/commons/WebTest.java
@@ -2,19 +2,16 @@ package me.saro.commons;
 
 import org.junit.jupiter.api.Test;
 
-//import me.saro.commons.web.Web;
+import me.saro.commons.web.Web;
 
 public class WebTest {
 
     @Test
     public void test() throws Exception {
-
-        // ssl cert ignore test
-        //		String sslErrorUrl = "https://cert-error.saro.me";
-        //		assertFalse(Web.get(sslErrorUrl).toPlainText().isSuccess());
-        //		assertTrue(Web.get(sslErrorUrl).setIgnoreCertificate(true).toPlainText().isSuccess());
-
-
+        
+        //System.out.println(Web.get("https://saro.me").toPlainText().getBody(null));
+        //System.out.println(Web.get("https://saro.me/error").toPlainText().getErrorBody(null));
+        
     }
 
 }
