432235e4c2a951850d0811e92ebee931ea41724c
201610099 <201610099@SKMNS-201610099.skmns.co.kr>
2019-04-17 18:48:53 +0900

    test save

--- src/test/java/me/saro/commons/FixedDataTest.java
+++ src/test/java/me/saro/commons/FixedDataTest.java
@@ -2,11 +2,7 @@ package me.saro.commons;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import me.saro.commons.bytes.Bytes;
+import me.saro.commons.bytes.FixedDataConverter;
 import me.saro.commons.bytes.FixedDataFormat;
 import me.saro.commons.bytes.annotations.FixedBinary;
 import me.saro.commons.bytes.annotations.FixedData;
@@ -22,74 +19,106 @@ import me.saro.commons.bytes.annotations.FixedTextAlign;
 
 public class FixedDataTest {
 
-    @Test
-    public void binary() {
-        FixedDataFormat<BinaryStruct> format = FixedDataFormat.getInstance(BinaryStruct.class);
-        
-        BinaryStruct bs = new BinaryStruct((byte)-1, (short)321, 1234, 76543L, 2.1F, 3.6D, new byte[] {0x1f, 0x3b, 0x33});
-        
-        byte[] bytes = format.toBytes(bs);
-        
-        assertEquals(bytes.length, 30);
-        
-        System.out.println(Bytes.toHex(bytes));
-        assertEquals(Bytes.toHex(bytes), "ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33");
-        
-        assertEquals(bs, format.toClass(bytes));
-        
-        byte[] bytes2 = new byte[60];
-        format.bindBytes(bytes2, 0, bs);
-        format.bindBytes(bytes2, 30, bs);
-        
-        System.out.println(Bytes.toHex(bytes2));
-        assertEquals(Bytes.toHex(bytes2), "ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33");
-        
-        assertEquals(bs, format.toClass(bytes2, 30));
-    }
+//    @Test
+//    public void binary() {
+//        FixedDataFormat<BinaryStruct> format = FixedDataFormat.getInstance(BinaryStruct.class);
+//        
+//        BinaryStruct bs = new BinaryStruct((byte)-1, (short)321, 1234, 76543L, 2.1F, 3.6D, new byte[] {0x1f, 0x3b, 0x33});
+//        
+//        byte[] bytes = format.toBytes(bs);
+//        
+//        assertEquals(bytes.length, 30);
+//        
+//        System.out.println(Bytes.toHex(bytes));
+//        assertEquals(Bytes.toHex(bytes), "ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33");
+//        
+//        assertEquals(bs, format.toClass(bytes));
+//        
+//        byte[] bytes2 = new byte[60];
+//        format.bindBytes(bytes2, 0, bs);
+//        format.bindBytes(bytes2, 30, bs);
+//        
+//        System.out.println(Bytes.toHex(bytes2));
+//        assertEquals(Bytes.toHex(bytes2), "ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33ff0141000004d20000000000012aff40066666400ccccccccccccd1f3b33");
+//        
+//        assertEquals(bs, format.toClass(bytes2, 30));
+//    }
+//    
+//    @Test
+//    public void text() throws UnsupportedEncodingException {
+//        FixedDataFormat<TextStruct> format = FixedDataFormat.getInstance(TextStruct.class);
+//        
+//        TextStruct ts = new TextStruct((byte)-1/* -1 == 255 */, (short)-321, 32123, -21L, 12.3F, -342.5D, "가나다", "abc");
+//        
+//        byte[] bytes = format.toBytes(ts);
+//        assertEquals(bytes.length, 100);
+//        
+//        String text = new String(bytes, "UTF-8");
+//        System.out.println(text);
+//        assertEquals(text, "255-321   0000007d7b-21                 12.3                -342.5              가나다        abc");
+//        
+//        TextStruct ts2 = format.toClass(text.getBytes("UTF-8"));
+//        System.out.println(ts2);
+//        assertEquals(ts, ts2);
+//    }
+//    
+//    @Test
+//    public void mixed() {
+//        FixedDataFormat<MixedStruct> format = FixedDataFormat.getInstance(MixedStruct.class);
+//        MixedStruct ms = new MixedStruct("Yong Seo", "PARK", 1);
+//        
+//        byte[] bytes = format.toBytes(ms);
+//        
+//        assertEquals(bytes.length, 34);
+//        
+//        System.out.println(Bytes.toHex(bytes));
+//        assertEquals(Bytes.toHex(bytes), "596f6e672053656f202020202020205041524b202020202020202020202000000001");
+//        
+//        assertEquals(ms, format.toClass(bytes, 0));
+//        
+//        System.out.println(ms);
+//        System.out.println(format.toClass(bytes, 0));
+//    }
+//    
+//    @Test
+//    public void mutipleThreadStored() {
+//        Utils.executeAllThreads(5, IntStream.range(0, 10).boxed().collect(Collectors.toList()), i -> {
+//           System.out.println("exec : " + i);
+//           array();
+//           return i;
+//        });
+//    }
+//    
+//    @Test
+//    public void array() {
+//        
+//        System.out.println("execute array");
+//        
+//        FixedDataFormat<ArrayStruct> format = FixedDataFormat.getInstance(ArrayStruct.class);
+//        ArrayStruct ms = new ArrayStruct(1, new int[] {2,3,4,5}, Arrays.asList(1L, -2L), new Short[] {21, 72});
+//        
+//        byte[] bytes = format.toBytes(ms);
+//        
+//        assertEquals(bytes.length, 40);
+//        
+//        System.out.println(Bytes.toHex(bytes));
+//        
+//        assertEquals(Bytes.toHex(bytes), "00000001000000020000000300000004000000050000000000000001fffffffffffffffe00150048");
+//        
+//        assertEquals(ms, format.toClass(bytes, 0));
+//        
+//        System.out.println(ms);
+//        System.out.println(format.toClass(bytes, 0));
+//    }
     
     @Test
-    public void text() throws UnsupportedEncodingException {
-        FixedDataFormat<TextStruct> format = FixedDataFormat.getInstance(TextStruct.class);
-        
-        TextStruct ts = new TextStruct((byte)-1/* -1 == 255 */, (short)-321, 32123, -21L, 12.3F, -342.5D, "가나다", "abc");
-        
-        byte[] bytes = format.toBytes(ts);
-        assertEquals(bytes.length, 100);
-        
-        String text = new String(bytes, "UTF-8");
-        System.out.println(text);
-        assertEquals(text, "255-321   0000007d7b-21                 12.3                -342.5              가나다        abc");
-        
-        TextStruct ts2 = format.toClass(text.getBytes("UTF-8"));
-        System.out.println(ts2);
-        assertEquals(ts, ts2);
-    }
-    
-    @Test
-    public void mixed() {
-        FixedDataFormat<MixedStruct> format = FixedDataFormat.getInstance(MixedStruct.class);
-        MixedStruct ms = new MixedStruct("Yong Seo", "PARK", 1);
-        
-        byte[] bytes = format.toBytes(ms);
-        
-        assertEquals(bytes.length, 34);
-        
-        System.out.println(Bytes.toHex(bytes));
-        assertEquals(Bytes.toHex(bytes), "596f6e672053656f202020202020205041524b202020202020202020202000000001");
-        
-        assertEquals(ms, format.toClass(bytes, 0));
-        
-        System.out.println(ms);
-        System.out.println(format.toClass(bytes, 0));
-    }
-    
-    @Test
-    public void array() {
+    public void inClass() {
         
         System.out.println("execute array");
         
-        FixedDataFormat<ArrayStruct> format = FixedDataFormat.getInstance(ArrayStruct.class);
-        ArrayStruct ms = new ArrayStruct(1, new int[] {2,3,4,5}, Arrays.asList(1L, -2L), new Short[] {21, 72});
+        FixedDataFormat<ParentStruct> format = FixedDataFormat.getInstance(ParentStruct.class);
+        //ParentStruct ms = new ParentStruct(1, new ChildStruct(2, "단일"), new ChildStruct[] {new ChildStruct(32, "배열1"), new ChildStruct(81, "배열2")});
+        ParentStruct ms = new ParentStruct(1, new ChildStruct(2, "단일"));
         
         byte[] bytes = format.toBytes(ms);
         
@@ -103,15 +132,6 @@ public class FixedDataTest {
         
         System.out.println(ms);
         System.out.println(format.toClass(bytes, 0));
-    }
-    
-    @Test
-    public void mutipleThreadStored() {
-        Utils.executeAllThreads(5, IntStream.range(0, 10).boxed().collect(Collectors.toList()), i -> {
-           System.out.println("exec : " + i);
-           array();
-           return i;
-        });
     }
     
     @Data
@@ -207,4 +227,35 @@ public class FixedDataTest {
         @FixedBinary(offset=36, arrayLength=2)
         Short[] member4;
     }
+    
+    @Data
+    //@FixedData(size=46)
+    @FixedData(size=40)
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ParentStruct {
+        
+        @FixedBinary(offset=0)
+        int main;
+        
+        @FixedBinary(offset=4)
+        ChildStruct ch;
+        
+        @FixedBinary(offset=18, arrayLength=2)
+        ChildStruct[] chArr;
+    }
+    
+    @Data
+    @FixedData(size=14)
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ChildStruct {
+        
+        @FixedBinary(offset=0)
+        int no;
+        
+        @FixedText(offset=4, length=10)
+        String text;
+    }
 }
+
