14df4e6dab34f88ae33bef7db7e60163e7731f96
PARK Yong Seo <j@saro.me>
2019-08-24 05:12:13 +0900

    lists

---
+++ src/main/java/me/saro/commons/v5/Lists.java
@@ -0,0 +1,63 @@
+package me.saro.commons.v5;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+/**
+ *
+ * @author  PARK Yong Seo
+ * @since   5.0
+ */
+public class Lists {
+
+    public static enum SortOption {
+        none,
+        asc,
+        desc
+    }
+
+
+    /**
+     * bind rank
+     * @param list
+     * @param sortOption
+     * @param comparator
+     * @param bindRank
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> bindRank(List<T> list, SortOption sortOption, Comparator<T> comparator, BiConsumer<T, Integer> bindRank) {
+
+        if (list.isEmpty()) {
+            return list;
+        }
+
+        // sort
+        switch (sortOption != null ? sortOption : SortOption.none) {
+            case asc:
+                list.sort(comparator);
+                break;
+            case desc:
+                list.sort((a, b) -> -comparator.compare(a, b));
+        }
+
+        int rank = 1;
+        int rankAcc = 1;
+        int index = 0;
+        T prev = list.get(0);
+        bindRank.accept(prev, rank);
+        while(++index < list.size()) {
+            T node = list.get(index);
+            if (comparator.compare(prev, node) == 0) {
+                rankAcc++;
+            } else {
+                rank += rankAcc;
+                rankAcc = 1;
+            }
+            bindRank.accept(node, rank);
+            prev = node;
+        }
+        return list;
+    }
+}
